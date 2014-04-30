84cd8a87089b15d10481b7d2dc97969e1845006c
Matt Stevenson <mattoc@mattoc.com>
2014-05-01 00:25:57 +1000

    Adds step 10

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import kivy
 kivy.require('1.7.1')
 
 from kivy.app import App
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
 
 class SayThis(BoxLayout):
-    pass
+    saywhat_text = ObjectProperty(None)
+
+    def clear(self):
+        self.saywhat_text.text = ''
+        self.saywhat_text.focus = True
 
 
 class SayThisApp(App):
